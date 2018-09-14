481217caf0287b334254d61673d3b0a2d18021cc
Leslie DE JAGER <lde-jage@e4r6p10.wethinkcode.co.za>
2018-09-14 09:29:58 +0200

    fix exceptions

--- include/GraphicsErrors.hpp
+++ include/GraphicsErrors.hpp
@@ -2,37 +2,70 @@
 #define GRAPHICSERRORS_HPP
 
 #include <string>
-#include <exception>
+#include <stdexcept>
 
 class GraphicsErrors
 {
     public:
-        class FileDoesntExist : public std::logic_error
+        class FileDoesntExist : public std::exception
         {
             public:
-                FileDoesntExist(const std::string& msg) : logic_error(std::string("File doesn't exist: ") + msg)
-                {}
+                FileDoesntExist(const std::string& msg) : std::exception()
+                {
+                    e_msg = std::string("File doesn't exist: ") + msg;
+                }
+                virtual const char* what() const throw()
+                {
+                    return e_msg.c_str();
+                }
+
+            private:
+                std::string e_msg;
         };
 
-        class IncorrectFileFormat : public std::logic_error
+        class IncorrectFileFormat : public std::exception
         {
             public:
-                IncorrectFileFormat(const std::string& msg) : logic_error(std::string("Incorrectly Formated File: ") + msg)
-                {}
+                IncorrectFileFormat(const std::string& msg) : std::exception()
+                {
+                    e_msg = std::string("Incorrectly Formated File: ") + msg;
+                }
+                virtual const char* what() const throw()
+                {
+                    return e_msg.c_str();
+                }
+            private:
+                std::string e_msg;
         };
 
-        class AssimpError : public std::logic_error
+        class AssimpError : public std::exception
         {
             public:
-                AssimpError(const std::string& msg) : logic_error(std::string("Assimp Error: ") + msg)
-                {}
+                AssimpError(const std::string& msg) : std::exception()
+                {
+                    e_msg = std::string("Assimp Error: ") + msg;
+                }
+                virtual const char* what() const throw()
+                {
+                    return e_msg.c_str();
+                }
+            private:
+                std::string e_msg;
         };
 
-        class TextureFailed : public std::logic_error
+        class TextureFailed : public std::exception
         {
             public:
-                TextureFailed(const std::string& msg) : logic_error(std::string("Texture failed to Load: ") + msg)
-                {}
+                TextureFailed(const std::string& msg) : std::exception()
+                {
+                    e_msg = std::string("Texture failed to Load: ") + msg;
+                }
+                virtual const char* what() const throw()
+                {
+                    return e_msg.c_str();
+                }
+            private:
+                std::string e_msg;
         };
 };
 
